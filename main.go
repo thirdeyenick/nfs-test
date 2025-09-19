97d13dc4c1bbf91874fe906b7540458fcaea51ff
Sebastian Nickel <nick@nine.ch>
2025-09-19 17:20:45 +0200

    more functionality

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"strconv"
@@ -22,13 +24,14 @@ type FileInfo struct {
 }
 
 var (
-	server      = os.Getenv("NFS_SERVER") // e.g. "192.168.1.50"
-	share       = os.Getenv("NFS_SHARE")  // e.g. "/export/data"
-	listenPort  = os.Getenv("PORT")
-	timeoutEnv  = os.Getenv("TIMEOUT")
-	machineName = os.Getenv("MACHINE_NAME")
-	uidEnv      = os.Getenv("UID")
-	gidEnv      = os.Getenv("GID")
+	server         = os.Getenv("NFS_SERVER") // e.g. "192.168.1.50"
+	share          = os.Getenv("NFS_SHARE")  // e.g. "/export/data"
+	listenPort     = os.Getenv("PORT")
+	timeoutEnv     = os.Getenv("TIMEOUT")
+	machineName    = os.Getenv("MACHINE_NAME")
+	uidEnv         = os.Getenv("UID")
+	gidEnv         = os.Getenv("GID")
+	storagePathEnv = os.Getenv("STORAGE_PATH")
 )
 
 func main() {
@@ -59,11 +62,54 @@ func main() {
 		log.Fatalf("can not parse GID env variable: %v", err)
 	}
 
+	logger := slog.Default()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	http.HandleFunc("/write-storage", func(w http.ResponseWriter, r *http.Request) {
+		response := response{writer: w, logger: logger}
+		defer response.handle()
+
+		if storagePathEnv == "" {
+			response.err = errors.New("no storage path set via STORAGE_PATH env variable")
+			return
+		}
+		// Get the current time
+		currentTime := time.Now().Format(time.RFC3339)
+
+		// Write time to file
+		err := os.WriteFile(storagePathEnv, []byte(currentTime), 0644)
+		if err != nil {
+			response.err = fmt.Errorf("error writing to file: %w", err)
+			return
+		}
+		response.Message = fmt.Sprintf("current time written to store: %s", currentTime)
+		return
+	})
+
+	http.HandleFunc("/read-storage", func(w http.ResponseWriter, r *http.Request) {
+		response := response{writer: w, logger: logger}
+		defer response.handle()
+
+		if storagePathEnv == "" {
+			response.err = errors.New("no storage path set via STORAGE_PATH env variable")
+			return
+		}
+		content, err := os.ReadFile(storagePathEnv)
+		if err != nil {
+			response.err = fmt.Errorf("error reading from file: %w", err)
+			return
+		}
+		response.Message = fmt.Sprintf("last time written to store: %s", string(content))
+		return
+	})
+
 	http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+		response := response{writer: w, logger: logger}
+		defer response.handle()
+
 		initCtx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
 
@@ -74,7 +120,7 @@ func main() {
 		}
 		client, err := nfs4.NewNfsClient(initCtx, server, auth)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("Error creating NFS client: %v", err), http.StatusInternalServerError)
+			response.err = fmt.Errorf("error creating NFS client: %w", err)
 			return
 		}
 
@@ -85,20 +131,16 @@ func main() {
 
 		entries, err := client.GetFileList(path)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("Error reading dir %q: %v", path, err), http.StatusInternalServerError)
+			response.err = fmt.Errorf("Error reading dir %q: %v", path, err)
 			return
 		}
-
-		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(entries); err != nil {
-			http.Error(w, fmt.Sprintf("can not convert to json output reading dir %q: %v", path, err), http.StatusInternalServerError)
-			return
-		}
+		response.Files = entries
 	})
 
-	log.Printf("Serving on %s (NFS %s:%s)", listenPort, server, share)
+	logger.Info("serving", "nfs-server", server, "nfs-share", share, "listen-address", listenPort)
 	if err := http.ListenAndServe(prefixPort(listenPort), nil); err != nil {
-		log.Fatalf("HTTP server error: %v", err)
+		logger.Error("HTTP server error", "error", err.Error())
+		os.Exit(1)
 	}
 }
 
@@ -107,4 +149,27 @@ func prefixPort(port string) string {
 		return port
 	}
 	return ":" + port
+}
+
+type response struct {
+	Error   string          `json:"error,omitempty"`
+	Message string          `json:"message,omitempty"`
+	Files   []nfs4.FileInfo `json:"files,omitempty"`
+	writer  http.ResponseWriter
+	logger  *slog.Logger
+	err     error
+}
+
+func (r response) handle() {
+	r.writer.Header().Set("Content-Type", "application/json")
+	if r.err != nil {
+		r.Error = "something went wrong"
+		r.logger.Error(r.err.Error())
+		r.writer.WriteHeader(http.StatusInternalServerError)
+	} else {
+		r.writer.WriteHeader(http.StatusOK)
+	}
+	if err := json.NewEncoder(r.writer).Encode(r); err != nil {
+		r.logger.Error("can not JSON encode output", "error", err.Error())
+	}
 }
